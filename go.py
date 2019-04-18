84b17d25752be3ea53b5f82420319af6f75b0590
Dan <dansonlinepresence@gmail.com>
2019-04-18 18:35:05 -0400

    update go.py to batch encrypt/decrypt

--- go.py
+++ go.py
@@ -4,16 +4,20 @@ import subprocess
 import sys
 
 direction = sys.argv[1]
-name = sys.argv[2]
-archive_name = '{}.zip'.format(name)
-cipherfile_name = '{}.enc'.format(name)
-
-if direction == 'e':
-    shutil.make_archive(name, 'zip', name)
+names = sys.argv[2:]
 
 password = getpass.getpass()
 
-subprocess.check_call(['node', 'javascrypt.node.js', direction, archive_name, cipherfile_name, password])
+for name in names:
+    print(name)
 
-if direction == 'd':
-    shutil.unpack_archive(archive_name, name)
+    archive_name = '{}.zip'.format(name)
+    cipherfile_name = '{}.enc'.format(name)
+
+    if direction == 'e':
+        shutil.make_archive(name, 'zip', name)
+
+    subprocess.check_call(['node', 'javascrypt.node.js', direction, archive_name, cipherfile_name, password])
+
+    if direction == 'd':
+        shutil.unpack_archive(archive_name, name)
